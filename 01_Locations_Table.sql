3962f18ec6ecd9bd18a97abaa09cf9f8b97df948
Sheenam Grover <sheenamkaura@gmail.com>
2023-08-11 14:30:09 +1000

    Updated Locations Table

--- 01_Locations_Table.sql
+++ 01_Locations_Table.sql
@@ -1,2 +1,7 @@
-
+Drop Table Locations
+GO
+create table Locations (Location_ID int primary key,RegionalGroup varchar(50))
+GO
 insert into Locations VALUES (122,'New York'),(123,'Dallas'),(124,'Chicago'),(167,'Boston')
+GO
+select * from Locations
